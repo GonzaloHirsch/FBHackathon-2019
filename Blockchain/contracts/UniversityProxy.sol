3f4c978da37ddef5098544b73661b2252d505dc9
GonzaloHirsch <hirschgonzalo@yahoo.com.ar>
2019-03-16 23:08:49 -0300

    Revert "Working UI"
    This reverts commit e7554e52654111b9bb77ccafa1e0d10322dc610c.

--- Blockchain/contracts/UniversityProxy.sol
+++ Blockchain/contracts/UniversityProxy.sol
@@ -49,7 +49,6 @@ contract UniversityProxy {
         universities[_universityID].courses[newCourse.id] = newCourse;
         universities[_universityID].activeCourses.push(newCourse.id);
         universities[_universityID].numberOfCourses = universities[_universityID].numberOfCourses + 1;
-        totalCourseCount++;
         return true;
     }
 
