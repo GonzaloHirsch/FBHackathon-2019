8c02c63f0118fd349903840badb1fdb6b0877669
Nacho <ivillanueva@itba.edu.at>
2019-03-16 22:14:19 -0300

    GetActiveCourses

--- Blockchain/contracts/StudentContract.sol
+++ Blockchain/contracts/StudentContract.sol
@@ -16,4 +16,38 @@ contract StudentContract {
         studentStorage.setCourseActive(_student, _course, false);
         studentStorage.setCourseGrade(_student, _course, _grade);
     }
+
+    function getActiveCourses(uint _student) view public returns(uint[] memory){
+        uint[] memory courses = studentStorage.getCourses(_student);
+        uint j = 0;
+        uint length = courses.length;
+        uint[] memory activeCourses = new uint[](length);
+        for(uint i = 0; i < length; i++){
+            if(studentStorage.getCourseActive(_student, courses[i])){
+                activeCourses[j] = (courses[i]);
+                j++;
+            }
+        }
+        courses = new uint[](j);
+        for(uint i = 0; i < j; i++)
+            courses[i] = activeCourses[i];
+        return courses;
+    }
+
+    function getNonActiveCourses(uint _student) view public returns(uint[] memory){
+        uint[] memory courses = studentStorage.getCourses(_student);
+        uint j = 0;
+        uint length = courses.length;
+        uint[] memory nonActiveCourses = new uint[](length);
+        for(uint i = 0; i < length; i++){
+            if(!studentStorage.getCourseActive(_student, courses[i])){
+                nonActiveCourses[j] = (courses[i]);
+                j++;
+            }
+        }
+        courses = new uint[](j);
+        for(uint i = 0; i < j; i++)
+            courses[i] = nonActiveCourses[i];
+        return courses;
+    }
 }
--- Blockchain/test/StudentTest.sol
+++ Blockchain/test/StudentTest.sol
@@ -12,8 +12,8 @@ contract StudentTest {
     StudentProxy sp;
     StudentContract sc;
     uint studentId = 1;
-    uint universityId = 2;
-    uint courseId = 3;
+    uint universityId = 1;
+    uint courseId = 1;
     uint grade = 10;
 
     function beforeAll() public{
@@ -33,15 +33,23 @@ contract StudentTest {
         Assert.equal(sp.getCourses(studentId)[0], courseId, "Course does not match");
         Assert.equal(sp.getCourseUniversity(studentId, courseId), universityId, "University does not match");
     }
-/* 
-    function testCourseProperties() {
-
-    } */
 
     function testEvaluateCourse() public{
         sc.evaluateCourse(studentId, courseId, grade);
         Assert.equal(sp.getCourseGrade(studentId, courseId), grade, "Grade does not match");
         Assert.equal(sp.getCourseActive(studentId, courseId), false, "Course is active after evaluation");
+    }
+
+    function testGetActiveCourses() public{
+        uint _student = 2;
+        sp.addStudent("Student", _student);
+        for(uint i = 0; i < 10; i++){
+            sp.addCourse(_student, 2, i);
+        }
+
+        for(uint i = 0; i < 10; i++){
+            Assert.equal(sc.getActiveCourses(_student)[i], i, "Active courses does not match");
+        }
     }
 
 
